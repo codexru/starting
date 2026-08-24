a18d1f9901ba78dada27447b768da19c17138f45
codexru <codexru@morke.ru>
2026-08-24 03:03:53 -0500

    Create new file

--- python/levelup_slow.py
+++ python/levelup_slow.py
@@ -1,2 +1,8 @@
 # Data management that includes input, string, and number (both decimals and integers)
-print
+print("Hi user")
+
+# input, that command used to enter data
+name = input("What's your name: ")
+# important: input, The type of data that comes out will always be a string or str
+print(type(name))
+print(f"Hi {name}, my name is corux")
