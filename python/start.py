08c139193a738f1a33b7826cfac992db15f36446
codexru <codexru@morke.ru>
2026-08-24 00:45:18 -0500

    add type of data and examples

--- python/start.py
+++ python/start.py
@@ -1,4 +1,41 @@
 print("hello world")
 
-calculator = 2+2
+calculator = 2 + 2
 print(calculator)
+
+
+
+# type of data
+
+# str
+string = "Text or string"
+print(type(string))
+
+# int
+integer = 3  # 3, 2, 1, -1, -2
+print(type(integer))
+
+# float
+float = 3.14 # 3.14, -2.5
+print(type(float))
+
+# bool True/False
+boolean = True # False
+print(type(boolean))
+
+# list
+list = [1, 2, 3]
+print(type(list))
+
+# tuple
+tuples = (1, 2, 3)
+print(type(tuples))
+
+# set
+sets = {1, 2, 3}
+print(type(sets))
+
+# dict
+dictionaries = {"clave": "valor"}
+print(type(dictionaries))
+
