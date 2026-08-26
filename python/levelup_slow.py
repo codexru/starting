f0735a43eaed1d53f9d58521d5eec72a97d2f1ab
codexru <codexru@morke.ru>
2026-08-26 01:16:46 -0500

    Restart to routes man

--- python/levelup_slow.py
+++ python/levelup_slow.py
@@ -3,6 +3,8 @@ print("Hi user")
 
 # input, that command used to enter data
 name = input("What's your name: ")
+age = input("What's your age: ")
 # important: input, The type of data that comes out will always be a string or str
-print(type(name))
-print(f"Hi {name}, my name is corux")
+print(type(name), type(age))
+print(f"Hi {name}, my name is corux. Your age is: {age}")
+
