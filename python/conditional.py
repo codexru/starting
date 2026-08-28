24b8840a2bdacd1999f1ebd48e9f2a368c28f537
codexru <codexru@morke.ru>
2026-08-28 01:12:20 -0500

    full

--- python/conditional.py
+++ python/conditional.py
@@ -4,3 +4,18 @@ clear("clear")
 print("\n###################\n Hello, i'm back\n###################\n")
 x = input(" My name is: ")
 print(f"\n good {x}, i'm corux\n ")
+if x == "admin":
+ print("motherfucker, lol, walcome sir")
+ x = 0
+ while x <= (10**5):
+  if x == 50:
+   y = input("break? Y/N: ")
+   y = y.upper()
+   if y == "Y":
+    break
+   elif x == "N":
+    print("ok")
+   else:
+    print("you have a brain")
+  print(x)
+  x = x+1
